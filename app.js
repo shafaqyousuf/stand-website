86aa5ddaa5fdf191b35da88e5365339291226b86
shafaq yousuf <shafaqarshad172@gmail.com>
2022-10-31 03:02:00 -0700

    assignment done

---
+++ app.js
@@ -0,0 +1,57 @@
+//  chapter 1
+
+alert("hello js");
+
+// chapter 2
+
+var a = ("my world")
+alert(a)
+
+// chapter 3
+
+var b = 18
+alert(b)
+
+// chapter 4
+// var var  // illegal
+// var 12  // illegal
+// var abc // legal
+// var ab12 // legal
+
+// chapter 5
+
+var c = 10
+var d = 20
+var e = c + d
+alert(e)
+
+// chapter 6
+
+var f = 10
+var g = f++
+var h = --g
+alert(g)
+alert(h)
+
+console.log(g)
+console.log(h)
+
+// chapter 7
+
+var i = c + d
+var j = c * d
+var k = c - d
+var l = c / d
+
+alert(i)
+alert(j)
+alert(k)
+alert(l)
+
+// chapter 8
+
+var m = c + d
+var n = a + c
+alert(m)
+alert(n)
+
