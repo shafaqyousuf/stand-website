44c17e08f672c2746d525df7fa43d61d4ed5774d
shafaq yousuf <shafaqarshad172@gmail.com>
2022-11-10 05:06:05 -0800

    assignment complete

--- app.js
+++ app.js
@@ -1,57 +1,83 @@
-//  chapter 1
+// //  chapter 1
 
-alert("hello js");
+// alert("hello js");
 
-// chapter 2
+// // chapter 2
 
-var a = ("my world")
-alert(a)
+// var a = ("my world")
+// alert(a)
 
-// chapter 3
+// // chapter 3
 
-var b = 18
-alert(b)
+// var b = 18
+// alert(b)
 
-// chapter 4
-// var var  // illegal
-// var 12  // illegal
-// var abc // legal
-// var ab12 // legal
+// // chapter 4
+// // var var  // illegal
+// // var 12  // illegal
+// // var abc // legal
+// // var ab12 // legal
 
-// chapter 5
+// // chapter 5
 
-var c = 10
-var d = 20
-var e = c + d
-alert(e)
+// var c = 10
+// var d = 20
+// var e = c + d
+// alert(e)
 
-// chapter 6
+// // chapter 6
 
-var f = 10
-var g = f++
-var h = --g
-alert(g)
-alert(h)
+// var f = 10
+// var g = f++
+// var h = --g
+// alert(g)
+// alert(h)
 
-console.log(g)
-console.log(h)
+// console.log(g)
+// console.log(h)
 
-// chapter 7
+// // chapter 7
 
-var i = c + d
-var j = c * d
-var k = c - d
-var l = c / d
+// var i = c + d
+// var j = c * d
+// var k = c - d
+// var l = c / d
 
-alert(i)
-alert(j)
-alert(k)
-alert(l)
+// alert(i)
+// alert(j)
+// alert(k)
+// alert(l)
 
-// chapter 8
+// // chapter 8
 
-var m = c + d
-var n = a + c
-alert(m)
-alert(n)
+// var m = c + d
+// var n = a + c
+// alert(m)
+// alert(n)
+
+
+// var eng = +prompt
+
+// chapter 15 to 20
+
+var z = ["a", "b", "c", "d", "e", "f"]
+
+z.push("y", "z")
+z.pop()
+z.shift()
+z.unshift("0")
+var x = z.slice(1 , 3)
+var y = z.splice(3 , 3)
+
+console.log(y)
+
+for (var s=1; s<11; s++ ){
+console.log("2 " + "x " + s + "= " + s*2)
+}
+
+
+
+
+
+
 
